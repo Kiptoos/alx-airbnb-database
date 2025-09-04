1414ce5962401718406e6aafe35034e203ccf7e3
KiptooStephen <kiptoos@gmail.com>
2025-09-04 22:31:59 +0300

    perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,14 +1,16 @@
 -- perfomance.sql
 
 -- Initial query: all bookings + user + property + payment details
-SELECT b.id AS booking_id, b.start_date, b.end_date, b.status,
+SELECT b.id AS booking_id,
+       b.start_date, b.end_date, b.status,
        u.id AS user_id, u.name AS user_name, u.email,
-       p.id AS property_id, p.title, p.nightly_price, p.city,
+       p.id AS property_id, p.title AS property_title, p.nightly_price, p.city,
        pay.id AS payment_id, pay.amount, pay.currency, pay.status AS payment_status, pay.paid_at
 FROM bookings b
 JOIN users u      ON u.id = b.user_id
 JOIN properties p ON p.id = b.property_id
 LEFT JOIN payments pay ON pay.booking_id = b.id;
+
 
 -- Analyze performance of the initial query
 EXPLAIN ANALYZE
